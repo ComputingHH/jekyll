ddef51b6ded461b3b1de911da8bcd069b99dbcf9
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2017-05-10 08:18:55 -0700

    More informative error message for missing fields

--- _plugins/exception.rb
+++ _plugins/exception.rb
@@ -1,9 +1,9 @@
 # Force liquid to raise an error and stop build if this filter is called
 module Jekyll
   module ExceptionFilter
-    def raise_error(input)
+    def raise_error(field)
     	bad_page = @context.registers[:page]['path']
-    	err_msg = "#{bad_page}: #{input}"
+    	err_msg = "YAML header for #{bad_page} has a missing or empty '#{field}' field."
       raise err_msg
     end
   end
